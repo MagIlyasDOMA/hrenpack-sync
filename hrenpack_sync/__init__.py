b3bcf72f22bae3d724b894bf9411f054a97938ee
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-03-18 22:33:38 +0300

    0.1.1

--- hrenpack_sync/__init__.py
+++ hrenpack_sync/__init__.py
@@ -1,10 +1,12 @@
 import os, sys
-from typing import Literal, List, Optional
+from typing import List, Optional
 from typer import Typer, Argument, Option
 from . import commands
 from .json_config import Config
 
 app = Typer(no_args_is_help=True)
+
+VALID_MANAGERS = ['uv', 'pipenv', 'npm', 'yarn', '*']  # Определяем допустимые значения
 
 
 @app.command
@@ -15,57 +17,99 @@ def clone(directory: str = Argument('hrenpack_source', help="Directory to clone"
 
 
 @app.command
-def managers(action: Optional[Literal['add', 'remove', 'set']] = Argument(None, help="Action to perform"),
-             args: Optional[List[Literal['uv', 'pipenv', 'npm', 'yarn', '*']]] = Argument(
-                 None, help="Managers list. Use 'remove *' for removing all managers")):
+def managers(
+        action: Optional[str] = Argument(None, help="Action to perform (add/remove/set)"),
+        args: Optional[List[str]] = Argument(None, help="Managers list. Use 'remove *' for removing all managers")
+):
+    """Manage package managers configuration"""
     config = Config()
     data = set(config['managers'])
+
+    # Показываем текущие менеджеры если action не указан
     if action is None:
         print(', '.join(data))
         sys.exit(0)
-    match action:
-        case 'add':
-            for arg in args: data.add(arg)
-        case 'remove':
-            if '*' in args: data = set()
-            else:
-                for arg in args:
-                    try: data.remove(arg)
-                    except KeyError: pass
-        case 'set': data = set(args)
-        case _: raise ValueError('Invalid action')
-    config['managers'] = set(data)
+
+    # Валидация action
+    if action not in ['add', 'remove', 'set']:
+        print(f"Error: Invalid action '{action}'. Use add, remove, or set", file=sys.stderr)
+        sys.exit(1)
+
+    # Валидация args
+    if args is None:
+        args = []
+
+    # Валидация значений менеджеров
+    for arg in args:
+        if arg not in VALID_MANAGERS:
+            print(f"Error: Invalid manager '{arg}'. Valid managers: {', '.join(VALID_MANAGERS)}", file=sys.stderr)
+            sys.exit(1)
+
+    # Выполнение действия
+    if action == 'add':
+        for arg in args:
+            data.add(arg)
+    elif action == 'remove':
+        if '*' in args:
+            data = set()
+        else:
+            for arg in args:
+                data.discard(arg)  # discard не вызывает ошибку если элемент отсутствует
+    elif action == 'set':
+        data = set(args)
+
+    # Сохраняем изменения
+    config['managers'] = list(data)  # Преобразуем set в list для JSON
+    print(f"Managers updated: {', '.join(data)}")
 
 
 @app.command
-def sync(hrenpack_only: bool = Option(False, '--hrenpack-only', '-H', 'o', help="Pull origin hrenpack only")):
+def sync(
+        hrenpack_only: bool = Option(False, '--hrenpack-only', '-H', help="Pull origin hrenpack only")
+):
+    """Sync repositories and install dependencies"""
     config = Config()
     code = 0
+
     if not hrenpack_only:
         code = os.system('git pull origin')
+
     if code == 0:
         os.chdir(config['directory'])
         code = os.system('git pull origin')
         os.chdir('..')
+
     for manager in config['managers']:
-        if code != 0: break
-        elif manager == 'uv': code = os.system(f'{manager} sync')
-        elif manager in ('pipenv', 'npm', 'yarn'): code = os.system(f'{manager} install')
+        if code != 0:
+            break
+        elif manager == 'uv':
+            code = os.system(f'{manager} sync')
+        elif manager in ('pipenv', 'npm', 'yarn'):
+            code = os.system(f'{manager} install')
+
     sys.exit(code)
 
 
 @app.command
-def commit(hrenpack_only: bool = Option(False, '--hrenpack-only', '-H', 'o', help="Commit hrenpack only")):
+def commit(
+        hrenpack_only: bool = Option(False, '--hrenpack-only', '-H', help="Commit hrenpack only")
+):
+    """Commit changes"""
     sys.exit(commands.commit(hrenpack_only))
 
 
 @app.command
-def push(hrenpack_only: bool = Option(False, '--hrenpack-only', '-H', 'o', help="Push origin hrenpack only"),
-         commit_: bool = Option(False, '--commit', '-c', '-C', help="Commit hrenpack only")):
-    if commit_:
+def push(
+        hrenpack_only: bool = Option(False, '--hrenpack-only', '-H', help="Push origin hrenpack only"),
+        commit: bool = Option(False, '--commit', '-c', help="Commit before pushing")
+):
+    """Push changes"""
+    if commit:
         code = commands.commit(hrenpack_only)
-        if code != 0: sys.exit(code)
+        if code != 0:
+            sys.exit(code)
     sys.exit(commands.push(hrenpack_only))
 
 
-if __name__ == '__main__': app()
+if __name__ == '__main__':
+    app()
